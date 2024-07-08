0e08b9ca270081b75349e5d96f940544af157f9b
2lefthandsyt <77549594+2lefthandsyt@users.noreply.github.com>
2024-07-08 12:22:45 -0400

    Update ninjaone.sh

--- ninjaone.sh
+++ ninjaone.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+
+sudo wget -O NinjaOne.deb https://app.ninjarmm.com/agent/installer/464eeb51-ad69-4a28-bf60-6e1ec61adad5/userendpointsproductiondevices-5.8.9154-installer-x86-64.deb
+
+sudo dpkg -i NinjaOne.deb
+
